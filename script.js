78b2d7e85c6b9bd80f5502b7e9b715dbd60f52a7
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-19 16:21:40 -0400

    Feat: the grid size is mutable
    - make the range input work
    - show the actual size

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
+// start the web page with a grid created
+createGridContainer(8);
 
-const userInpt = 16 
-const totalOfDivs = Math.pow(userInpt, 2);
-const gridContainerSize = 500;
-const gridElSize = Math.floor(gridContainerSize / userInpt);
-console.log(gridElSize)
+const changeSize =document.querySelector('#choseSize').addEventListener('input', e => {
+  const SIZE = e.target.value;
+  showActualSize(SIZE);
+  cleanGridContainer();
+  createGridContainer(SIZE);
+});
 
-for (let i = 0; i < totalOfDivs; i++){
-  // create div
-  let gridEl = document.createElement('div');
-  gridEl.classList.add('grid-element');
-  gridEl.style.height = `${gridElSize}px`;
-  gridEl.style.width = `${gridElSize}px`;
-  gridEl.addEventListener('mouseover', e => {
-    e.target.style.background = '#000'
-  });
-  // put div in the container
-  document.querySelector('.grid-container').appendChild(gridEl);
+
+function createGridContainer(userInput) {
+  const totalOfDivs = getNumOfDiv(userInput);
+  const gridElSize = getGridElSize(userInput);
+
+  for (let i = 0; i < totalOfDivs; i++){
+    // create div
+    let gridEl = document.createElement('div');
+    gridEl.classList.add('grid-element');     // set div classes
+    gridEl.classList.add('el-border')
+    gridEl.style.height = `${gridElSize}px`;  // set the correct height & width
+    gridEl.style.width = `${gridElSize}px`;
+    gridEl.addEventListener('mouseover', e => {
+      e.target.style.background = '#000'
+    });
+    // put div in the container
+    document.querySelector('.grid-container').appendChild(gridEl);
+  }
+}
+
+function cleanGridContainer(){
+  document.querySelector('.grid-container').innerHTML = '';
+}
+
+function showActualSize(size){
+  document.querySelector('.actual-size').textContent = `${size} x ${size}`
+}
+
+function getGridElSize(userInput){
+  const gridContainerSize = 500;
+  return gridElSize = gridContainerSize / userInput;
+}
+
+function getNumOfDiv(userInput){
+  return Math.pow(userInput, 2);
 }
